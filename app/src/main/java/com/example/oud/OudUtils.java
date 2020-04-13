7352a036dc13e2402a4df28cdc2442ac1c5dd216
Yaamani <mahmoud_medhat.2008@yahoo.com>
2020-04-13 14:12:38 +0200

    generic instead of string

--- app/src/main/java/com/example/oud/OudUtils.java
+++ app/src/main/java/com/example/oud/OudUtils.java
@@ -7,17 +7,17 @@ public class OudUtils {
     public static <T> String commaSeparatedListQueryParameter(ArrayList<T> list) {
         StringBuilder stringBuilder = new StringBuilder();
         for (T item : list) {
-            stringBuilder.append(item);
+            stringBuilder.append(item.toString());
             stringBuilder.append(',');
         }
         stringBuilder.deleteCharAt(stringBuilder.length()-1);
         return stringBuilder.toString();
     }
 
-    public static String commaSeparatedListQueryParameter(String[] list) {
+    public static <T> String commaSeparatedListQueryParameter(T[] list) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (String item : list) {
-            stringBuilder.append(item);
+        for (T item : list) {
+            stringBuilder.append(item.toString());
             stringBuilder.append(',');
         }
         stringBuilder.deleteCharAt(stringBuilder.length()-1);
