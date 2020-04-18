3088f08cb99b60a9bd8fbf72491613e8733966ae
youssefEdrees <44654408+youssefEdrees@users.noreply.github.com>
2020-04-18 22:26:35 +0200

    convert image to full url

--- app/src/main/java/com/example/oud/OudUtils.java
+++ app/src/main/java/com/example/oud/OudUtils.java
@@ -2,6 +2,7 @@ package com.example.oud;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ public class OudUtils {
             stringBuilder.append(item.toString());
             stringBuilder.append(',');
         }
-        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         return stringBuilder.toString();
     }
 
@@ -26,49 +27,62 @@ public class OudUtils {
             stringBuilder.append(item.toString());
             stringBuilder.append(',');
         }
-        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         return stringBuilder.toString();
     }
 
-    public static void saveUserData(View v , String token,String userId){
+    public static void saveUserData(View v, String token, String userId) {
 
         SharedPreferences prefs = v.getContext().getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
 
-        token = "Bearer "+token;
+        token = "Bearer " + token;
 
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(Constants.SHARED_PREFERENCES_TOKEN_NAME,token);
-        prefsEditor.putString(Constants.SHARED_PREFERENCES_USER_ID_NAME,userId);
+        prefsEditor.putString(Constants.SHARED_PREFERENCES_TOKEN_NAME, token);
+        prefsEditor.putString(Constants.SHARED_PREFERENCES_USER_ID_NAME, userId);
 
         prefsEditor.apply();    //token saved in shared preferences
 
     }
 
-    public static void saveUserData(Context context, String token,String userId){
+    public static void saveUserData(Context context, String token, String userId) {
 
         SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
 
-        token = "Bearer "+token;
+        token = "Bearer " + token;
 
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(Constants.SHARED_PREFERENCES_TOKEN_NAME,token);
-        prefsEditor.putString(Constants.SHARED_PREFERENCES_USER_ID_NAME,userId);
+        prefsEditor.putString(Constants.SHARED_PREFERENCES_TOKEN_NAME, token);
+        prefsEditor.putString(Constants.SHARED_PREFERENCES_USER_ID_NAME, userId);
         prefsEditor.apply();    //token saved in shared preferences
 
     }
-    public static String getToken(Context context){
-    SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
-    return  prefs.getString(Constants.SHARED_PREFERENCES_TOKEN_NAME,"");
 
-    }
-
-    public static String getUserId(Context context){
+    public static String getToken(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
-        return  prefs.getString(Constants.SHARED_PREFERENCES_USER_ID_NAME,"");
+        return prefs.getString(Constants.SHARED_PREFERENCES_TOKEN_NAME, "");
+
+    }
+
+    public static String getUserId(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
+        return prefs.getString(Constants.SHARED_PREFERENCES_USER_ID_NAME, "");
 
     }
 
 
+    public static String convertImageToFullUrl(String imageUrl) {
 
+        imageUrl = ("http://oud-zerobase.me/api/" + imageUrl);
 
+        for (int i = 0; i < imageUrl.length(); i++) {
+            if (imageUrl.charAt(i) == (char) 92) {
+                Log.e("profile fragment", imageUrl.charAt(i) + " at position: " + i);
+                StringBuilder tempString = new StringBuilder(imageUrl);
+                tempString.setCharAt(i, '/');
+                imageUrl = tempString.toString();
+            }
+        }
+        return imageUrl;
+    }
 }
